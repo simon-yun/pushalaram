bb9dd93f423b671e367a01edbd5da9b64dbcf6d7
drysm <oenpice2@gmail.com>
2021-08-23 22:00:22 +0900

    프로젝트 사용할 유아이 구성
    rtl ltl
    관련 -->startmargin 등등

--- app/src/main/java/com/example/pushalaram/MainActivity.kt
+++ app/src/main/java/com/example/pushalaram/MainActivity.kt
@@ -2,8 +2,18 @@ package com.example.pushalaram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resulttextview)
+    }
+
+    private val firebaseToken: TextView by lazy {
+        findViewById(R.id.firebasetoken)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
