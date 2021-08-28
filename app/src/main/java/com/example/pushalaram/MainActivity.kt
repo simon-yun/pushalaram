630c3e9dc4b9d38a45fac4aa5452403df8cee179
drysm <oenpice2@gmail.com>
2021-08-28 22:03:47 +0900

    아놔 리스타트  하니 커밋 내용 다 날라가네 google-services.json 이 파일은 올리지 말라.,. ( 앱을 컨트롤 할 수 있는 내용이 담겨저 잇어서 .. ) build.gradle 이 망할
    symc 하면서 없는 파일이면 안된다고 아니면 오류 메세지라도 날려주지...
    
    오타 잇는데 개무시함
    
    아놔 가상머신 씨밤 ,, 무슨 adb C 언어 뭐 가  안된다고
    안됨..
    그냥 속 편하게 휴대폰으로 하는것을 추천

--- app/src/main/java/com/example/pushalaram/MainActivity.kt
+++ app/src/main/java/com/example/pushalaram/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.pushalaram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,12 +12,24 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.resulttextview)
     }
 
-    private val firebaseToken: TextView by lazy {
-        findViewById(R.id.firebasetoken)
+    private val firebasetokenview: TextView by lazy {
+        findViewById(R.id.firebasetokenview)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val token = task.result
+                    firebasetokenview.text = task.result
+                }
+            }
+
     }
 }
